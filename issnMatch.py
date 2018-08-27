47c183af46da664145b464571358275745916c60
Sengupta <nirvan.sengupta@clarivate.com>
2018-08-27 10:55:58 -0400

    new file

---
+++ issnMatch.py
@@ -0,0 +1,8 @@
+"""
+Author: Nirvan Sengupta
+Date: August 27th, 2018
+Filename: issnMatch.py
+Description: 
+Script to find matching ISSNs in SSCI and SCIE metrics files from a list of ISSNs provided by client. 
+Returns JIFs for 200 ISSNs when they are found. 
+"""
