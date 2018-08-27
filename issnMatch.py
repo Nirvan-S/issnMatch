00b74651bca66615f7ea0eaf8728f9ac63c4f87c
Nirvan-S <nirvan.sengupta@gmail.com>
2018-08-27 12:42:57 -0400

    read metrics data

--- issnMatch.py
+++ issnMatch.py
@@ -11,4 +11,14 @@ import pandas as pd
 
 ### Read Client's Data ###
 
+
 ### Read SSCI and SCIE data ###
+inputDir = "C:\\Users\\u6048427\\OneDrive - Clarivate Analytics\\General\\"
+
+print("Reading SCIE JCR Metrics file")
+scie = pd.read_excel( io = inputDir + 'JCR_SCIE_2017.xlsx')
+scie = scie[['ISSN','IMPACT_FACTOR']]
+
+print("Reading SSCI JCR Metrics file")
+ssci = pd.read_excel( io = inputDir + 'JCR_SSCI_2017.xlsx')
+ssci = ssci[['ISSN','IMPACT_FACTOR']]
